c72f4755b5f2d00354847d91836d5709f58122f7
Jared McDonald <jared.r.mcdonald@gmail.com>
2014-02-01 03:57:34 -0500

    cleanup

--- public/js/new-ingredient.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function(){
-//     $('#newIngredient').on('submit', function submitForm(e){
-//         e.preventDefault();
-//         var $this = $(this);
-//         $.post($this.attr('action'), $this.serialize());
-//     });
-// });
